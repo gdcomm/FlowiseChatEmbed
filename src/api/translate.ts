4a20a80b0ba0b26e9e3380be50706942e062b751
gdcomm-leeleo <cslee@gdcomm.io>
2024-01-12 12:17:13 +0900

    :art:

--- src/api/translate.ts
+++ src/api/translate.ts
@@ -2,31 +2,29 @@ import axios from 'axios';
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  user: 'gdcomm',
-  host: 'database-postgresql-1.cnyg6emkyn2a.ap-northeast-2.rds.amazonaws.com',
-  database: 'postgres',
-  password: 'gdcomm18!!',
-  port: 5432,
+    user: 'gdcomm',
+    host: 'database-postgresql-1.cnyg6emkyn2a.ap-northeast-2.rds.amazonaws.com',
+    database: 'postgres',
+    password: 'gdcomm18!!',
+    port: 5432,
 });
 
 export async function getApiKey(): Promise<string> {
-  try {
-    const result = await pool.query('SELECT secret_key FROM auth WHERE active = true');
-    if (result.rows.length > 0) {
-      return result.rows[0].secret_key;
+    try {
+        const result = await pool.query('SELECT secret_key FROM auth WHERE active = true');
+        if (result.rows.length > 0) {
+            return result.rows[0].secret_key;
+        }
+        return '';
+    } catch (error) {
+        console.error('Error fetching API key from the database:', error);
+        return '';
     }
-    return '';
-  } catch (error) {
-    console.error('Error fetching API key from the database:', error);
-    return '';
-  }
 }
 
 async function translateWithGPT3(from: string, to: string, text: string): Promise<string> {
   const apiUrl = 'https://api.openai.com/v1/engines/gpt-3.5-turbo-instruct/completions';
-
-  // Get API key from the database
-  const apiKey = await getApiKey();
+  const apiKey = await getApiKey()
 
   if (!apiKey) {
     console.error('API key not found in the database');
