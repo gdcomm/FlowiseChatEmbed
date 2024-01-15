b296b125a0fd4df26e15957669f8bb3ad52c0c1c
gdcomm-leeleo <cslee@gdcomm.io>
2024-01-15 16:59:19 +0900

    test

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -23,6 +23,7 @@ export type MessageType = {
   sourceDocuments?: any;
   fileAnnotations?: any;
   test?: string;
+  check?: string;
 };
 
 export type BotProps = {
@@ -121,7 +122,7 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
   // Handle errors
   const handleError = (message = isKorean() ? '잠시 후 다시 시도 해주시기 바랍니다.' : 'Oops! There seems to be an error. Please try again.') => {
     setMessages((prevMessages) => {
-      const messages: MessageType[] = [...prevMessages, { message, type: 'apiMessage', test: '1' }];
+      const messages: MessageType[] = [...prevMessages, { message, type: 'apiMessage' }];
       addChatMessage(messages);
       return messages;
     });
@@ -195,7 +196,7 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
               sourceDocuments: data?.sourceDocuments,
               fileAnnotations: data?.fileAnnotations,
               type: 'apiMessage',
-              test: '2',
+              test: '1',
             },
           ];
           addChatMessage(messages);
@@ -224,7 +225,6 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
         {
           message: props.welcomeMessage ?? defaultWelcomeMessage,
           type: 'apiMessage',
-          test: '3',
         },
       ]);
     } catch (error: any) {
@@ -294,7 +294,7 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
     });
 
     socket.on('start', () => {
-      setMessages((prevMessages) => [...prevMessages, { message: '', type: 'apiMessage', test: '4' }]);
+      setMessages((prevMessages) => [...prevMessages, { message: '', type: 'apiMessage', check: '1' }]);
     });
 
     socket.on('sourceDocuments', updateLastMessageSourceDocuments);
@@ -309,7 +309,6 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
         {
           message: props.welcomeMessage ?? defaultWelcomeMessage,
           type: 'apiMessage',
-          test: '5',
         },
       ]);
       if (socket) {
