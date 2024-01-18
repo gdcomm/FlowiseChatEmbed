bbe3e8fdd6d9ec200950827dfde643b473c81d8b
gdcomm-leeleo <cslee@gdcomm.io>
2024-01-18 11:10:41 +0900

    start

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -312,11 +312,10 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
 
     console.log('socket branch: ' + JSON.stringify(messages()));
 
-
     // eslint-disable-next-line solid/reactivity
     socket.on('start', () => {
-      console.log('start check')
-      console.log('start isKorean : ' + isKorean())
+      console.log('start check');
+      console.log('start isKorean : ' + isKorean());
       setMessages((prevMessages) => [...prevMessages, { message: '', type: 'apiMessage' }]);
     });
 
